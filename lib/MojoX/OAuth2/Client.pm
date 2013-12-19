715be3bcc3c3a37d0a7a33675ef55b02c8f34079
Chris Smith <csmith1@companieshouse.gov.uk>
2013-12-19 16:01:09 +0000

    Clean up.

--- lib/MojoX/OAuth2/Client.pm
+++ lib/MojoX/OAuth2/Client.pm
@@ -1,78 +1,190 @@
-package Mojolicious::Plugin::MangoWrapper;
+package MojoX::OAuth2::Client;
 
-our $VERSION = '0.01';
+use Mojo::Base 'Mojo::EventEmitter';
+use Mojo::UserAgent;
 
-use Mojolicious::Plugin::MangoWrapper::CollectionWrapper;
-use Mojo::Base 'Mojolicious::Plugin';
+has 'operations' => sub { [] };
+has 'code';
+has 'state';
 
-has 'mango';
 
-sub register {
-    my ($self, $app, $options) = @_;
+has 'identity_providers';
+has '_provider';
 
-    $self->mango($options->{mango});
+#-------------------------------------------------------------------------------
 
-    $app->helper(mongodb => sub {
-        my ($controller, $db, $collection) = @_;
-        return $self->wrap_mango($db, $collection);
-    });
+sub on {
+    my ($self, %events) = @_;
+
+    for my $event (keys %events) {
+        $self->SUPER::on($event => $events{$event});
+    }
+
+    return $self;
 }
 
-sub wrap_mango {
-    my ($self, $db, $collection) = @_;
+#-------------------------------------------------------------------------------
 
-    ($db, $collection) = $db =~ /([^\.]+)\.([^\.]+)/ if $db =~ /\./ and !$collection;
+sub provider {
+    my ($self, $provider) = @_;
 
-    return new Mojolicious::Plugin::MangoWrapper::CollectionWrapper->new(
-        collection => $self->mango->db($db)->collection($collection)
-    );
+    # Set IDP config hash as provider
+    $self->_provider( $self->identity_providers->{$provider} );
+
+    return $self;
 }
+
+#-------------------------------------------------------------------------------
+
+sub get_authorization_url
+{
+    my ($self, %args) = @_;
+
+    my $redirect_url  = $args{redirect_url}  // $self->_provider->{redirect_url};
+    my $response_type = $args{response_type} // 'code';
+    my $scope         = $args{scope}         // $self->_provider->{scope} // '';
+    my $client_id     = $self->_provider->{client_id};
+    my $auth_url      = $self->_provider->{authorize_url};
+
+    delete $args{redirect_url};
+    delete $args{response_type};
+    delete $args{scope};
+
+    # authorisation request is a GET
+    my $url = Mojo::URL->new( $auth_url );
+
+    # Mandatory parameters
+    $url->query->append( client_id     => $client_id     );
+    $url->query->append( redirect_url  => $redirect_url  );
+    $url->query->append( response_type => $response_type );
+
+    # Optional parameters
+    $url->query->append( scope => $scope         ) if $scope;
+
+    # Append any further args that may have been passed in, including state.
+    foreach my $opt_arg (keys %args)
+    {
+        $url->query->append( $opt_arg => $args{$opt_arg} );
+    }
+
+    return $url;
+}
+
+#-------------------------------------------------------------------------------
+
+sub execute {
+    my ($self) = @_;
+
+    die("No operation") unless @{$self->operations};
+
+    
+    while( !$self->error && @{$self->operations} )
+    {
+        shift->();
+    }
+}
+
+
+#-------------------------------------------------------------------------------
+# Params passed back from OAuth2 server will be x-form-url-encoded
+#
+sub recieve_code {
+    my ($self, $params) = @_;
+
+    push @{$self->operations}, sub
+    {
+        my $errors = $self->_extract_errors( $params );
+
+        $self->code ( $params->{code} );
+        $self->state( $params->{state} ) if $params->{state};
+
+        # If we are not chaining to a get_token operation, deal with callbacks here.
+        if( not @{$self->operations} ) {
+            return $self->emit_safe('access_denied' => $errors ) 
+                    if $errors && $errors->{error} eq 'access_denied' && $self->has_subscribers('access_denied');
+            return $self->emit_safe('failure' => $errors) if $errors;
+            return $self->emit_safe('success' => $self->result );
+        }
+    };
+
+    return $self;
+}
+
+#-------------------------------------------------------------------------------
+
+sub get_token {
+    my ($self, %args) = @_;
+
+    push @{$self->operations}, sub {
+        my $ua = Mojo::UserAgent->new;
+
+        my $params;
+        my $grant_type = $args{grant_type} || 'authorization_code';
+        my $assertion  = $args{assertion};
+
+        if( $grant_type eq 'authorization_code' )
+        {
+            $params = {
+                code          => $args{code} // $self->code,
+                client_id     => $self->_provider->{client_id},
+                client_secret => $self->_provider->{client_secret},
+                redirect_url  => $self->_provider->{redirect_url},
+                grant_type    => $grant_type
+            };
+        }
+
+        if( $assertion )
+        {
+            $params = {
+                assertion  => $assertion,
+                grant_type => $grant_type
+                # 'urn:ietf:params:oauth:grant-type:jwt-bearer'
+            };
+        }
+
+
+        die "unsupported grant_type '$grant_type'" unless $params;
+
+        $ua->on( error => sub {
+            my( $ua, $err ) = @_;
+            # TODO make $err consistent with all other data passed to failure callback
+            return $self->emit_safe('failure' => $err );
+        } );
+
+        $ua->post( $self->_provider->{token_url}, form => $params => sub {
+            my ($client, $tx) = @_;
+                if( my $res = $tx->success ) { return $self->emit_safe('success' => $res->json ) };
+
+                my $errors = $self->_extract_errors( $tx->error ); # FIXME  what  does $tx->error look like??
+
+                return $self->emit_safe('failure' => $errors );
+            }
+        );
+    };
+
+    return $self;
+}
+
+#-------------------------------------------------------------------------------
+
+sub _extract_errors
+{
+    my ($self, $response) = @_;
+
+    my %errors;
+    if( $response->{error} )
+    {
+        $errors{error}             = $response->{error};
+        $errors{error_description} = $response->{error_description} if $response->{error_description};
+        $errors{error_uri}         = $response->{error_uri}         if $response->{error_uri};
+        #  FIXME Add status code for other types of failure
+    }
+
+    $self->error( \%errors );
+
+    return \%errors;
+}
+
+#-------------------------------------------------------------------------------
 
 1;
-
-=encoding utf8
-
-=head1 NAME
-
-Mojolicious::Plugin::MangoWrapper- A Mojolicious wrapper for Mango
-
-=head1 DESCRIPTION
-
-L<Mojolicious::Plugin::MangoWrapper> provides a wrapper to L<Mango> and
-L<Mango::Collection> objects to provide a cleaner interface and to
-enforce the use of non-blocking L<Mango> calls.
-
-=head1 ATTRIBUTES
-
-L<Mojolicious::Plugin::MangoWrapper> implements the following attributes.
-
-=head2 collection
-
-    my $collection = $wrapper->collection;
-    $queue->collection($mango->db('foo')->collection('bar'));
-
-The L<Mango::Collection> used by chained operations.
-
-=head1 METHODS
-
-L<Mojolicious::Plugin::MangoWrapper> implements the following methods.
-
-=head2 register
-
-    $app = Mojolicious->new;
-    $wrapper->register($app, { mango => Mango->new });
-
-Registers a Mojolicious helper method to return a wrapped L<Mango::Collection>.
-
-=head2 wrap_mango
-
-    $wrapped = $wrapper->wrap_mango('db', 'collection');
-    $wrapped = $wrapper->wrap_mango('db.collection');
-
-Wraps a L<Mango::Collection> for the provided database and collection.
-
-=head1 SEE ALSO
-
-L<Mojolicious>, L<Mango>
-
-=cut
